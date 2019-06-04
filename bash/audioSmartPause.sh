2adea8be0073c78528050e47d5c3d19b0136ec62
DonSimonIV <47987359+DonSimonIV@users.noreply.github.com>
2019-06-04 12:34:02 +0200

    Update audioSmartPause.sh

--- bash/audioSmartPause.sh
+++ bash/audioSmartPause.sh
@@ -1,34 +1,29 @@
 #!/bin/bash
 
-# kodi-send --host=192.168.1.33 --port=9777 --action='pause'
-#cmus-remote -c -U
-
-#programs
+#Not tested, Just works with cmus
+#Add IP to kodi-send
+#programs that output audio
 number=$(fuser /dev/snd/controlC0 | wc -w)
-echo "$number"
 
 if [[ "$number" = "0" ]]; then
-  echo ""
+  exit
 fi
 
 elif [[ "$number" = "1" ]]; then
-	echo "this"
+        #Get the PID of the programs
  	pid=$(basename -a $(fuser /dev/snd/controlC0))
- 	name=$(basename -a $(ls -l /proc/$pid/exe | cut -d ' ' -f 12 ))
-	echo "$name"  
-
-	if [[ "$name" = "cmus" ]]; then
-		echo "good"    
+ 	
+        #Get their command name
+        name=$(basename -a $(ls -l /proc/$pid/exe | cut -d ' ' -f 12 ))
+  
+	if [[ "$name" = "cmus" ]]; then  
 		cmus-remote -c -U
 	fi
 
-	elif [[ "$name = "kodi-x11" ]]; then
-		kodi-send --host=192.168.1.33 --port=9777 --action='pause'
-#	fi
-#else
-#fi
-#elif [[ "$number" >= "2" ]]; then
-#	kodi-send --host=192.168.1.33 --port=9777 --action='pause'
-#	cmus-remote -c -U
+	if [[ "$name = "kodi-x11" ]]; then
+		kodi-send --host=192.168.1.XX --port=9777 --action='pause'
+        fi
+else
+	kodi-send --host=192.168.1.XX --port=9777 --action='pause'
+	cmus-remote -c -U
 	
-fi
